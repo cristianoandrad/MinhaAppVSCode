03530d6e8137788d0b532fd0235a10a61bbcf0a1
cristianoandrad <cristianocontabil@gmail.com>
2021-08-09 10:41:10 -0300

    primeiro commit

---
+++ MeuMiddleware.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+public class MeuMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public MeuMiddleware(RequestDelegate next){
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context){
+        Console.WriteLine("\n\r -------- Antes -------- \n\r");
+
+        await _next(context);
+
+        Console.WriteLine("\n\r -------- Depois -------- \n\r");
+    }
+    
+}
+
+public static class MeuMiddlewareExtension
+{
+    public static IApplicationBuilder UseMeuMiddleware(this IApplicationBuilder builder ){
+        return builder.UseMiddleware<MeuMiddleware>();
+    }
+}
